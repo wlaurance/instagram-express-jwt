1b216f5d9e4d93fb23fd14929e4290d74f76cdf7
wlaurance <w.laurance@gmail.com>
2014-11-17 19:06:29 -0500

    WIP basic server
    add secrets to gitignore

---
+++ index.js
@@ -0,0 +1,39 @@
+var express = require('express')
+var passport = require('passport')
+var InstagramStrategy = require('passport-instagram').Strategy
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var env = process.env;
+var host = env.HOST;
+var INSTAGRAM_CLIENT_SECRET = env.IG_SECRET;
+var INSTAGRAM_CLIENT_ID = env.IG_ID;
+var REDIRECT_URL = env.REDIRECT_URL;
+
+passport.use(new InstagramStrategy({
+    clientID: INSTAGRAM_CLIENT_ID,
+    clientSecret: INSTAGRAM_CLIENT_SECRET,
+    callbackURL: host + "/oauth_redirect"
+  },
+  function igCallback(accessToken, refreshToken, profile, done) {
+
+  }
+));
+
+var app = express();
+
+app.get('/', passport.authenticate('instagram'), function(){});
+
+function success(req, res) {
+  res.redirect(REDIRECT_URL);
+}
+
+app.get('/oauth_redirect', passport.authenticate('instagram'), success);
+
+app.listen(process.env.PORT || 3000);
